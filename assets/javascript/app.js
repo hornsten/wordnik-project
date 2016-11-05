1d77848fa2abfc51d86307abd7d4a0b2db2cab31
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-05 00:25:25 -0500

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,7 @@
-// var words = ['awesome', 'stellar', 'cool', 'unique', 'sassy', 'wonderful', 'interesting', 'talented'];
-
-// for (var i = 0; i < words.length; i++) {
-//     getPronunciation(words[i]);
-// }
-
 function getPronunciation(word) {
 
     var pathway = 'http://api.wordnik.com:80/v4/word.json/';
-    var parameters = '/audio?useCanonical=true&limit=50';
+    var parameters = '/audio?useCanonical=true&limit=10';
     var apiKey = '&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
 
     var queryURL = pathway + word + parameters + apiKey;
@@ -17,15 +11,14 @@ function getPronunciation(word) {
         method: 'GET'
     }).done(function(response) {
 
+        console.log(response);
         var theSound = response[0].fileUrl;
+
+        audio = new Audio(theSound);
         var theText = response[0].word;
-        $('#word').append(theText + " ");
-        $('#sound').append(' <audio controls class="say"><source src="' + theSound + '" type="audio/mp3"></audio>');
 
-
-
-
-
+        $('#sound').append('<h3>' + theText + '</h3>')
+            .append('<audio controls source src="' + theSound + '" type="audio/mp3"></audio>');
     })
 
 }
@@ -45,4 +38,6 @@ $(document).ready(function() {
         $("#text").val('');
 
     });
+
+
 });
